ff62a23487ac7e20bfa8a733d4c3bcd6e5aaf43d
Rudra570917 <rudrabhavsar26@gmail.com>
2026-02-09 12:52:20 +0530

    File modified

---
+++ Files/script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+function myfun(){
+    alert("This page says something");
+}
+document.getElementById("heading");
+let div=document.querySelector("div");
+console.dir(div);
+let h2=document.querySelector("h2");
+h2.innerText=h2.innerText+"from Apna College";
