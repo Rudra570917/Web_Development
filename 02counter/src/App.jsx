e35f67adcb8e59469fa263df0fff7bfd3394b254
Rudra Devangkumar Bhavsar <rudrabhavsar26@gmail.com>
2026-08-19 15:40:08 +0530

    Counter part added

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [counter,setCounter] = useState(15);
+  const addValue = () => {
+    console.log("value added",Math.random());
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    console.log("value removed",Math.random());
+    setCounter(counter - 1);
+  }
+ 
+
+  return (
+    <>
+      <h1>React JS</h1>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <br/>
+      <button onClick={removeValue} >Remove value</button>
+    </>
+  )
+}
+
+export default App
